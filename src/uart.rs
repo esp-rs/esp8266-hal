aa977317f673f0f991ebde65d85aa714b2321ad7
Robin Appelman <robin@icewind.nl>
2020-02-23 02:33:52 +0100

    add basic serial read

--- src/uart.rs
+++ src/uart.rs
@@ -1,4 +1,4 @@
-use embedded_hal::serial::Write;
+use embedded_hal::serial::{Read, Write};
 use esp8266::UART;
 use void::Void;
 
@@ -29,5 +29,20 @@ impl Write<u8> for Serial {
     fn flush(&mut self) -> nb::Result<(), Self::Error> {
         // noop
         Ok(())
+    }
+}
+
+impl Read<u8> for Serial {
+    /// Read error
+    type Error = Void;
+
+    /// Reads a single word from the serial interface
+    fn read(&mut self) -> nb::Result<u8, Self::Error> {
+        let state = self.uart.uart_status.read();
+        if state.uart_rxfifo_cnt().bits() > 0 {
+            Ok(self.uart.uart_fifo.read().uart_rxfifo_rd_byte().bits())
+        } else {
+            Err(nb::Error::WouldBlock)
+        }
     }
 }
