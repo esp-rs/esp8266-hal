481db1b923eae2cc296678a48732df2d05f2f320
Robin Appelman <robin@icewind.nl>
2020-02-29 17:52:50 +0100

    make timer2 periodic and merge timer implementations into a macro

--- src/timer.rs
+++ src/timer.rs
@@ -1,5 +1,4 @@
 use embedded_hal::timer::{Cancel, CountDown, Periodic};
-use esp8266::timer::frc1_ctrl::{INTERRUPT_TYPE_A, PRESCALE_DIVIDER_A};
 use esp8266::TIMER;
 use void::Void;
 
@@ -16,123 +15,99 @@ impl TimerExt for TIMER {
     }
 }
 
-pub struct Timer1 {
-    ticks_per_ms: u32,
-}
-
-impl Timer1 {
-    fn new(frequency: u32) -> Self {
-        let timer = unsafe { (&*TIMER::ptr()) };
-        timer.frc1_ctrl.write(|w| {
-            w.rollover()
-                .set_bit()
-                .interrupt_type()
-                .level()
-                .prescale_divider()
-                .devided_by_256()
-        });
-
-        Timer1 {
-            ticks_per_ms: (1_000_000_000 / (frequency / 256)),
+macro_rules! impl_timer {
+    ($TIMER:ident: ($ctrl:ident, $load:ident, $alarm:ident, $int:ident, $clr_mask:ident, $load_value:ident)) => {
+        pub struct $TIMER {
+            ticks_per_ms: u32,
         }
-    }
-}
 
-impl CountDown for Timer1 {
-    type Time = Nanoseconds;
+        impl $TIMER {
+            fn new(frequency: u32) -> Self {
+                let timer = unsafe { (&*TIMER::ptr()) };
+                timer.$ctrl.write(|w| {
+                    w.rollover()
+                        .set_bit()
+                        .interrupt_type()
+                        .level()
+                        .prescale_divider()
+                        .devided_by_256()
+                });
+                timer.$alarm.write(|w| unsafe { w.bits(0) });
 
-    fn start<T>(&mut self, timeout: T)
-    where
-        T: Into<Nanoseconds>,
-    {
-        let timer = unsafe { (&*TIMER::ptr()) };
-        let timeout: Nanoseconds = timeout.into();
-
-        let ticks = timeout.0 / self.ticks_per_ms;
-        timer.frc1_ctrl.modify(|_, w| w.timer_enable().set_bit());
-        timer.frc1_load.write(|w| unsafe { w.bits(ticks) });
-    }
-
-    fn wait(&mut self) -> nb::Result<(), Void> {
-        let timer = unsafe { (&*TIMER::ptr()) };
-        if timer.frc1_ctrl.read().frc1_int().bit_is_clear() {
-            Err(nb::Error::WouldBlock)
-        } else {
-            timer
-                .frc1_int
-                .modify(|_, w| w.frc1_int_clr_mask().set_bit());
-            Ok(())
+                $TIMER {
+                    ticks_per_ms: (1_000_000_000 / (frequency / 256)),
+                }
+            }
         }
-    }
-}
 
-impl Periodic for Timer1 {}
+        impl CountDown for $TIMER {
+            type Time = Nanoseconds;
 
-impl Cancel for Timer1 {
-    type Error = Void;
+            fn start<T>(&mut self, timeout: T)
+            where
+                T: Into<Nanoseconds>,
+            {
+                let timer = unsafe { (&*TIMER::ptr()) };
+                let timeout: Nanoseconds = timeout.into();
 
-    fn cancel(&mut self) -> Result<(), Self::Error> {
-        let timer = unsafe { (&*TIMER::ptr()) };
-        timer.frc1_ctrl.modify(|_, w| w.timer_enable().clear_bit());
-        Ok(())
-    }
-}
+                let ticks = timeout.0 / self.ticks_per_ms;
+                timer.$ctrl.modify(|_, w| w.timer_enable().set_bit());
+                timer.$load.write(|w| unsafe { w.bits($load_value(ticks)) });
+            }
 
-pub struct Timer2 {
-    ticks_per_ms: u32,
-}
-
-impl Timer2 {
-    fn new(frequency: u32) -> Self {
-        let timer = unsafe { (&*TIMER::ptr()) };
-        timer.frc2_ctrl.write(|w| {
-            w.interrupt_type()
-                .level()
-                .prescale_divider()
-                .devided_by_256()
-        });
-
-        Timer2 {
-            ticks_per_ms: (1_000_000_000 / (frequency / 256)),
+            fn wait(&mut self) -> nb::Result<(), Void> {
+                let timer = unsafe { (&*TIMER::ptr()) };
+                if timer.$ctrl.read().$int().bit_is_clear() {
+                    Err(nb::Error::WouldBlock)
+                } else {
+                    timer.$int.modify(|_, w| w.$clr_mask().set_bit());
+                    Ok(())
+                }
+            }
         }
-    }
-}
 
-impl CountDown for Timer2 {
-    type Time = Nanoseconds;
+        impl Periodic for $TIMER {}
 
-    fn start<T>(&mut self, timeout: T)
-    where
-        T: Into<Nanoseconds>,
-    {
-        let timer = unsafe { (&*TIMER::ptr()) };
-        let timeout: Nanoseconds = timeout.into();
+        impl Cancel for $TIMER {
+            type Error = Void;
 
-        let ticks = timeout.0 / self.ticks_per_ms;
-        timer.frc2_ctrl.modify(|_, w| w.timer_enable().set_bit());
-        timer.frc2_load.write(|w| unsafe { w.bits(0) });
-        timer.frc2_alarm.write(|w| unsafe { w.bits(ticks) });
-    }
-
-    fn wait(&mut self) -> nb::Result<(), Void> {
-        let timer = unsafe { (&*TIMER::ptr()) };
-        if timer.frc2_ctrl.read().frc2_int().bit_is_clear() {
-            Err(nb::Error::WouldBlock)
-        } else {
-            timer
-                .frc2_int
-                .modify(|_, w| w.frc2_int_clr_mask().set_bit());
-            Ok(())
+            fn cancel(&mut self) -> Result<(), Self::Error> {
+                let timer = unsafe { (&*TIMER::ptr()) };
+                timer.$ctrl.modify(|_, w| w.timer_enable().clear_bit());
+                Ok(())
+            }
         }
-    }
+    };
 }
 
-impl Cancel for Timer2 {
-    type Error = Void;
-
-    fn cancel(&mut self) -> Result<(), Self::Error> {
-        let timer = unsafe { (&*TIMER::ptr()) };
-        timer.frc2_ctrl.modify(|_, w| w.timer_enable().clear_bit());
-        Ok(())
-    }
+fn timer1_load_value(ticks: u32) -> u32 {
+    ticks
 }
+
+fn timer2_load_value(ticks: u32) -> u32 {
+    // timer2 counts up
+    u32::max_value() - ticks
+}
+
+impl_timer!(
+    Timer1:
+        (
+            frc1_ctrl,
+            frc1_load,
+            frc1_load,
+            frc1_int,
+            frc1_int_clr_mask,
+            timer1_load_value
+        )
+);
+impl_timer!(
+    Timer2:
+        (
+            frc2_ctrl,
+            frc2_load,
+            frc2_alarm,
+            frc2_int,
+            frc2_int_clr_mask,
+            timer2_load_value
+        )
+);
