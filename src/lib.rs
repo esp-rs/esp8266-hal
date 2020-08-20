c80df7a8c92d119af035ece3cedf67cc365d6001
Jesse Braham <jesse@beta7.io>
2020-08-19 18:19:59 -0700

    Add a prelude, re-export PAC

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 #![no_std]
 
 pub use embedded_hal as ehal;
+pub use esp8266 as target;
+
 pub mod gpio;
+pub mod prelude;
 pub mod timer;
 pub mod uart;
---
+++ src/prelude.rs
@@ -0,0 +1,17 @@
+//! The prelude.
+//!
+//! To use `esp8266_hal` effectively, a number of traits and types need to be
+//! imported. Instead of importing them one by one manually, the prelude
+//! contains the most commonly used imports that are used around application
+//! runtime management.
+//!
+//! This can be imported as use `esp8266_hal::prelude::*`.
+
+pub use crate::gpio::GpioExt;
+
+pub use embedded_hal::digital::v2::InputPin as _;
+pub use embedded_hal::digital::v2::OutputPin as _;
+pub use embedded_hal::digital::v2::StatefulOutputPin as _;
+pub use embedded_hal::digital::v2::ToggleableOutputPin as _;
+pub use embedded_hal::prelude::*;
+pub use embedded_hal::timer::{Cancel, CountDown, Periodic};
