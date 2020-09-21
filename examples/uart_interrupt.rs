2bffe2fd79429141faf7a207fe533dbd2409c1ba
Robin Appelman <robin@icewind.nl>
2020-09-21 14:51:11 +0200

    use static mutex to make uart interrupt example safe

--- examples/uart_interrupt.rs
+++ examples/uart_interrupt.rs
@@ -4,6 +4,10 @@
 use esp8266_hal::prelude::*;
 use esp8266_hal::target::Peripherals;
 use panic_halt as _;
+use xtensa_lx::mutex::{CriticalSectionMutex, Mutex};
+use esp8266_hal::gpio::{Gpio2, Output, PushPull};
+
+static LED: CriticalSectionMutex<Option<Gpio2<Output<PushPull>>>> = CriticalSectionMutex::new(None);
 
 #[entry]
 fn main() -> ! {
@@ -13,6 +17,10 @@ fn main() -> ! {
     let mut led = pins.gpio2.into_push_pull_output();
     let (mut timer1, _) = dp.TIMER.timers();
 
+    led.set_high().unwrap();
+
+    (&LED).lock(|tx_locked| *tx_locked = Some(led));
+
     // returned handler needs to be assigned to a variable to keep it in scope
     let _handler = serial.attach_interrupt(|serial| {
         if let Ok(byte) = serial.read() {
@@ -20,19 +28,14 @@ fn main() -> ! {
         }
     });
 
-    led.set_high().unwrap();
-
     loop {
         timer1.delay_ms(500);
-        led.toggle().unwrap();
+        (&LED).lock(|led| led.as_mut().unwrap().toggle().unwrap());
     }
 }
 
 /// You can both use the attached interrupt callback and set a function as interrupt handler
 #[interrupt(uart)]
 fn uart() {
-    let dp = unsafe { Peripherals::steal() };
-    let pins = dp.GPIO.split();
-    let mut led = pins.gpio2.into_push_pull_output();
-    led.toggle().unwrap();
+    (&LED).lock(|led| led.as_mut().unwrap().toggle().unwrap());
 }
