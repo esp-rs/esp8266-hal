a01e4c110f5eba3a0e157e11b5cad3bc2743a566
Robin Appelman <robin@icewind.nl>
2020-09-26 20:59:21 +0200

    add write! example to serial example

--- examples/serial.rs
+++ examples/serial.rs
@@ -4,12 +4,18 @@
 use esp8266_hal::prelude::*;
 use esp8266_hal::target::Peripherals;
 use panic_halt as _;
+use core::fmt::Write;
 
 #[entry]
 fn main() -> ! {
     let dp = Peripherals::take().unwrap();
     let pins = dp.GPIO.split();
     let mut serial = dp.UART0.serial(pins.gpio1.into_uart(), pins.gpio3.into_uart());
+
+    let (mut timer1, _) = dp.TIMER.timers();
+    timer1.delay_ms(100);
+
+    write!(serial, "\r\nstart:\r\n").unwrap();
 
     loop {
         let byte = nb::block!(serial.read()).unwrap();
